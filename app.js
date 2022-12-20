0b133edfb9000618b54b776e2aa3dba3e6ab1e23
IRUTHAYA SANTHOSE I <70632464+iruthayasanthose@users.noreply.github.com>
2022-12-20 12:00:50 +0530

    2nd Commmit

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ var objs = new Array("assets/gopher.png", "assets/bomb.png");
 
 const sound = new Audio("assets/smash.mp3");
 
+function gameOver() {
+  // const bombBlast =
+
+  document.getElementById("game").style.filter = "blur(50px)";
+  document.getElementById("game").style.backdropFilter = "blur(50px)";
+
+  document.getElementById("homebtns").style.filter = "blur(50px)";
+  document.getElementById("homebtns").style.backdropFilter = "blur(50px)";
+
+  document.getElementById("gameover").style.display = "block";
+  document.getElementById("gameover").style.zIndex = "1001";
+  document.getElementById("gameover").style.position = "fixed";
+  document.getElementById("gameover").style.top = "30%";
+  document.getElementById("gameover").style.left = "50%";
+  document.getElementById("gameover").style.transform = "translate(-50%, 0)";
+
+  // bombBlast =
+}
+
 function peep() {
   const i = Math.floor(Math.random() * holes.length);
   const hole = holes[i];
@@ -28,10 +47,12 @@ function peep() {
       img.src = "assets/dead_gopher.png";
     }
     if (obj[randN] == obj[1]) {
-      score += 5;
+      score = 0;
       sound.play();
       scoreEl.textContent = score;
       img.src = "assets/explode.png";
+
+      gameOver();
     }
     clearTimeout(timer);
     setTimeout(() => {
@@ -52,17 +73,19 @@ function startGame() {
   peep();
 }
 
-window.addEventListener("mousemove", (e) => {
-  cursor.style.top = e.pageY + "px";
-  cursor.style.left = e.pageX + "px";
-});
-window.addEventListener("mousedown", () => {
-  cursor.classList.add("active");
-});
-window.addEventListener("mouseup", () => {
-  cursor.classList.remove("active");
-});
+// HAmmer Code
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.pageY + "px";
+//   cursor.style.left = e.pageX + "px";
+// });
+// window.addEventListener("mousedown", () => {
+//   cursor.classList.add("active");
+// });
+// window.addEventListener("mouseup", () => {
+//   cursor.classList.remove("active");
+// });
 
 function resetGame() {
-  document.getElementsByClassName("score").reset();
+  window.location.reload();
+  return false;
 }
